86bf79f5b9d5c42025faad29f9d2d43468190b58
csikie <cs.erik18@gmail.com>
2021-11-11 20:51:01 +0100

    Update Lesson9.hs

--- csop26/Lesson9.hs
+++ csop26/Lesson9.hs
@@ -1,2 +1,28 @@
 module Lesson9 where
 
+import Data.List
+
+dropSpaces :: String -> String
+dropSpaces s = dropWhile (==' ') s
+
+trim :: String -> String -- reverse
+trim s =  reverse (dropSpaces (reverse (dropSpaces s)))
+
+monogram :: String -> String -- unwords - words | monogram "Jim Carrey" == "J. C."
+monogram s =  unwords (map f (words s)) where
+    f (x:_) = [x, '.']
+
+uniq :: Ord a => [a] -> [a]
+uniq l = map head (group (sort l))
+
+repeated :: Ord a => [a] -> [a]
+repeated l = map head (filter p (group (sort l))) where
+    p s = length s > 1
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+dotProduct :: [Int] -> [Int] -> Int
+dotProduct l1 l2 = sum (zipWith' (*) l1 l2)
