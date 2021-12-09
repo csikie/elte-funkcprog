43618770326b44b0052276c261c205f94efcb9d7
csikie <cs.erik18@gmail.com>
2021-12-09 07:46:32 +0100

    Update Vizsga.hs

--- csop5/Vizsga.hs
+++ csop5/Vizsga.hs
@@ -47,3 +47,47 @@ replace _ c [] = [c]
 replace n c (x:xs)
   | n == 0 = c : xs
   | otherwise = x : replace (n-1) c xs
+
+paripos :: [Int] -> Bool -- all/rekurzio, zip
+paripos l = and [mod (fst x) 2 == mod (snd x) 2 | x <- (zip [0..] l)]
+
+safeDiv :: Int -> Int -> Maybe Int
+safeDiv _ 0 = Nothing
+safeDiv a b = Just (div a b)
+
+parseCSV :: String -> [String] -- takeWhile-dropWhile/ span
+parseCSV [] = [""]
+parseCSV s = f (span (/=';') s) where
+  f (x, []) = [x]
+  f (x, y) = x : parseCSV (tail y)
+
+c :: (a -> b -> c) -> (b -> a -> c)
+c = flip
+
+selectUnless :: (t -> Bool) -> (t -> Bool) -> [t] -> [t] -- filter
+selectUnless p1 p2 l = filter (\x -> p1 x && not (p2 x)) l
+
+w :: (a -> a -> a) -> a -> a
+w f x = f x x
+
+
+-- x `f` x `f` ... `f` x `f` x
+ntimes :: (a -> a -> a) -> a -> Int -> a
+ntimes _ x 1 = x
+ntimes f x n = f x (ntimes f x (n-1))
+-- ntimes f val n = foldl f val [val | x <- [1..n-1]]
+--       (*) 2  4 =       (*) 2 [2,2,2]
+--        (((2*2)*2)*2)
+
+data Binary = On | Off
+  deriving (Eq, Show)
+
+switch :: Binary -> Binary
+switch On = Off
+switch _ = On
+
+bxor :: [Binary] -> [Binary] -> [Binary]
+bxor l1 l2 = zipWith xor l1 l2 where
+  xor x y
+    | x == y = On
+    | otherwise = Off
